c891f1b1c083b287f1754ee2c74082e2573fb350
raotnameh <raotnameh@gmail.com>
2025-04-02 17:20:33 +0530

    updated

--- src/better_train.py
+++ src/better_train.py
@@ -19,10 +19,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import yaml
-from torch.utils.data import DataLoader, SequentialSampler, BatchSampler
+from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.tensorboard import SummaryWriter
-
+from torch.cuda.amp import autocast, GradScaler
 
 # Local imports
 from dataset_speech import Dataset_speech
@@ -83,16 +83,7 @@ def configure_logging(dir='logs/') -> None:
 # step :- Prepare the dataset.
 def initialize_datasets(config: Dict) -> Tuple[DataLoader, DataLoader, Dict]:
     """Initialize and configure speech/text datasets with samplers."""
-    class ShuffledBatchSampler(BatchSampler):
-        """Custom batch sampler that shuffles batch order while maintaining sequence order within batches."""
-        def __init__(self, sampler, batch_size, drop_last):
-            super().__init__(sampler, batch_size, drop_last)  
-
-        def __iter__(self):
-            batches = list(super().__iter__())  
-            random.shuffle(batches)  # Shuffle batch order
-            return iter(batches)
-
+    
     # step 1 :- Prepare the speech dataset.
     speech_dataset = Dataset_speech(
         input_manifest=config['dataset_speech']['path'],
@@ -102,13 +93,10 @@ def initialize_datasets(config: Dict) -> Tuple[DataLoader, DataLoader, Dict]:
 
     speech_loader = DataLoader(
         speech_dataset,
-        batch_sampler=ShuffledBatchSampler(
-            sampler=SequentialSampler(speech_dataset),
-            batch_size=config['dataset_speech']['batch_size'],
-            drop_last=False,
-        ),
+        batch_size=config['dataset_txt']['batch_size'],
         collate_fn=speech_dataset.collate_fn,
         pin_memory=True,
+        shuffle=True,
         num_workers=6
     )
 
@@ -116,13 +104,10 @@ def initialize_datasets(config: Dict) -> Tuple[DataLoader, DataLoader, Dict]:
     text_dataset = Dataset_txt(data=config['dataset_txt']['path'])
     text_loader = DataLoader(
         text_dataset,
-        batch_sampler=ShuffledBatchSampler(
-            sampler=SequentialSampler(text_dataset),
-            batch_size=config['dataset_txt']['batch_size'],
-            drop_last=False
-        ),
+        batch_size=config['dataset_txt']['batch_size'],
         collate_fn=text_dataset.collate_fn,
         pin_memory=True,
+        shuffle=True,
         num_workers=6
     )
 
@@ -184,9 +169,7 @@ def setup_models(config: Dict, vocab: nn.Module) -> Dict:
 # step :- Prepare the training mode
 def configure_training_mode(models: Dict, config: Dict) -> None:
     """Set model training modes and parameter requirements."""
-    # Freeze codebook
-    # models['codebook'].embedding.weight.requires_grad_(False)
-    
+ 
     # Partially freeze encoder
     for name, param in models['encoder'].named_parameters():
         for n in config['encoder']['frozen_layers']:
@@ -306,6 +289,9 @@ def train(models: Dict, optimizers: Dict, schedulers:Dict, speech_loader: DataLo
     # Initialize TensorBoard writer
     writer = SummaryWriter(log_dir=config['logging'].get('log_dir', './logs'))
     
+    # Initialize GradScaler
+    scaler = GradScaler(enabled=config['train']['mixed_precision'])
+    
     num_steps = config['train']['num_steps']
     freeze_steps = config['train']['freeze_steps']
     
@@ -322,6 +308,9 @@ def train(models: Dict, optimizers: Dict, schedulers:Dict, speech_loader: DataLo
     
     loss_module.gan_loss.training = True
 
+    for optimizer in optimizers.values():
+        optimizer.zero_grad()
+    
     for step in range(start_step, num_steps + 1):
         output = {}
         
@@ -336,121 +325,138 @@ def train(models: Dict, optimizers: Dict, schedulers:Dict, speech_loader: DataLo
         
     
         # ===== Generator Forward Pass =====
-        enc_out = models['encoder'](waveforms, padding_masks)  # [B, T, C] # step 1
-        output["cnn_out"] = enc_out['cnn_out'] # [B, T // 320] 
-        output['encoder_out'] = enc_out['encoder_out'] # [B, T // 320] 
-        
-        mask = ~enc_out['padding_mask'] # B,T//320,1 # 0 for masked positions.
-        mask = mask.unsqueeze(-1).float()
-        output['enc_mask'] = mask
-        
-        # ===== Ground Truth =====
-        with torch.no_grad():
-            gt = models['gtruth'].encode(waveforms.unsqueeze(1)) # [B, T//320, 1024] 
-            gt = gt[:,:mask.shape[1],:] * mask # [B, T, 1024]
-            output['gt'] = gt 
-        
-        # ===== Generator Forward Pass Cont. =====
-        down_out = models['downsample'](enc_out['encoder_out']) # [B, T // 2, C] 
-        dmask = mask[:, ::config["upsample"]['stride']] # [B, T // config["upsample"]['stride'], 1]
-        down_out = down_out[:,:dmask.shape[1],:] * dmask # [B, T // 2, C]
-        output['down_out'] = down_out
-        output['dmask'] = dmask
-        
-        commitment_loss, diversity_loss, z_q, z_q_disc, encoding_indices, non_repeated_min_encoding_indices, non_repeated_mask = models['tokenizer'](
-            down_out, 
-            models['codebook'], 
-            dmask
-        ) # [B, T // 2, C], [B, T // 2, C], [B, T // 2] # step 3 -- all the necessary masks are already applied in the tokenizer
-        output['commitment_loss'] = commitment_loss
-        output['diversity_loss'] = diversity_loss
-        output['z_q'] = z_q
-        output['z_q_disc'] = z_q_disc
-        
-        up_out = models['upsample'](z_q)
-        up_out = up_out[:,:mask.shape[1],:] * mask # [B, T, C]       
-        output['up_out'] = up_out
-        
-        dec_out, dec_out2, dec_mask = models['decoder'](
-            x=up_out,
-            mask=enc_out['padding_mask'],
-            s=enc_out['cnn_out'],
-            use_s=config['decoder']['speaker']['use_s']
-        )
-        dec_out = dec_out[:,:dec_mask.shape[1],:] * dec_mask
-        dec_out2 = dec_out2[:,:dec_mask.shape[1],:] * dec_mask
-        output['dec_out'] = dec_out
-        output['dec_out2'] = dec_out2
-        output['dec_mask'] = dec_mask
-        
-        # Generator discriminator prediction
-        disc_fake = models['discriminator'](z_q_disc, non_repeated_mask.unsqueeze(-1))
-        output['disc_fake'] = disc_fake
-        
-        # Loss calculation
-        gen_loss_components = loss_module.step_gen(output)
-        total_lossg = sum(gen_loss_components.values())
-        
-        if step % config['logging']['step'] == 0:    
-            logging.info(f"GEN-LOSS---step/total: {step}/{num_steps} rec_loss: {gen_loss_components['rec_loss']}, commit_loss: {gen_loss_components['commit_loss']}, smooth_loss: {gen_loss_components['smooth_loss']}, gen_loss: {gen_loss_components['gen_loss']}, diversity_loss: {gen_loss_components['diversity_loss']}, total_loss: {total_lossg}")
+        with autocast(enabled=config['train']['mixed_precision']):
+            enc_out = models['encoder'](waveforms, padding_masks)  # [B, T, C] # step 1
+            output["cnn_out"] = enc_out['cnn_out'] # [B, T // 320] 
+            output['encoder_out'] = enc_out['encoder_out'] # [B, T // 320] 
             
-            writer.add_scalar('generator_loss/rec_loss', gen_loss_components['rec_loss'], step)
-            writer.add_scalar('generator_loss/commit_loss', gen_loss_components['commit_loss'], step)
-            writer.add_scalar('generator_loss/smooth_loss', gen_loss_components['smooth_loss'], step)
-            writer.add_scalar('generator_loss/gen_loss', gen_loss_components['gen_loss'], step)
-            writer.add_scalar('generator_loss/diversity_loss', gen_loss_components['diversity_loss'], step)
-            writer.add_scalar('generator_loss/total_loss_gen', total_lossg, step)
-        
-        
-        
-        # ===== Discriminator Forward Pass =====
-        # Get text batch (every 2 steps) with auto-reset
-        if step % config['train']['discriminator_freq'] == 0:
+            mask = ~enc_out['padding_mask'] # B,T//320,1 # 0 for masked positions.
+            mask = mask.unsqueeze(-1).float()
+            output['enc_mask'] = mask
             
-            z_q_disc = z_q_disc.clone().detach()
-            disc_fake = models['discriminator'](z_q_disc, non_repeated_mask.unsqueeze(-1).clone().detach())
+            # ===== Ground Truth =====
+            with torch.no_grad():
+                gt = models['gtruth'].encode(waveforms.unsqueeze(1)) # [B, T//320, 1024] 
+                gt = gt[:,:mask.shape[1],:] * mask # [B, T, 1024]
+                output['gt'] = gt 
+            
+            # ===== Generator Forward Pass Cont. =====
+            down_out = models['downsample'](enc_out['encoder_out']) # [B, T // 2, C] 
+            dmask = mask[:, ::config["upsample"]['stride']] # [B, T // config["upsample"]['stride'], 1]
+            down_out = down_out[:,:dmask.shape[1],:] * dmask # [B, T // 2, C]
+            output['down_out'] = down_out
+            output['dmask'] = dmask
+            
+            commitment_loss, diversity_loss, z_q, z_q_disc, encoding_indices, non_repeated_min_encoding_indices, non_repeated_mask = models['tokenizer'](
+                down_out, 
+                models['codebook'], 
+                dmask
+            ) # [B, T // 2, C], [B, T // 2, C], [B, T // 2] # step 3 -- all the necessary masks are already applied in the tokenizer
+            output['commitment_loss'] = commitment_loss
+            output['diversity_loss'] = diversity_loss
+            output['z_q'] = z_q
+            output['z_q_disc'] = z_q_disc
+            
+            up_out = models['upsample'](z_q)
+            up_out = up_out[:,:mask.shape[1],:] * mask # [B, T, C]       
+            output['up_out'] = up_out
+            
+            dec_out, dec_out2, dec_mask = models['decoder'](
+                x=up_out,
+                mask=enc_out['padding_mask'],
+                s=enc_out['cnn_out'],
+                use_s=config['decoder']['speaker']['use_s']
+            )
+            dec_out = dec_out[:,:dec_mask.shape[1],:] * dec_mask
+            dec_out2 = dec_out2[:,:dec_mask.shape[1],:] * dec_mask
+            output['dec_out'] = dec_out
+            output['dec_out2'] = dec_out2
+            output['dec_mask'] = dec_mask
+            
+            # Generator discriminator prediction
+            disc_fake = models['discriminator'](z_q_disc, non_repeated_mask.unsqueeze(-1))
             output['disc_fake'] = disc_fake
-            output["disc_fake_x"] = z_q_disc
-        
-            try:
-                text, tmask = next(text_iter)
-            except StopIteration:
-                text_iter = iter(text_loader)
-                text, tmask = next(text_iter)
-            text = text.to(device)
-            tmask = tmask.to(device)
             
-            text_emb = models['codebook'](text)
-            disc_real = models['discriminator'](text_emb, tmask.unsqueeze(-1))
-            output['disc_real'] = disc_real
-            output['disc_real_x'] = text_emb
-
-
-     
-            # Discriminator training
-            loss_module.gan_loss.discriminator = models['discriminator']
+            # Loss calculation
+            gen_loss_components = loss_module.step_gen(output)
+            total_lossg = sum(gen_loss_components.values())
+            
+            if step % config['logging']['step'] == 0:    
+                logging.info(
+                f"GEN-LOSS---step/total: {step}/{num_steps} "
+                f"rec_loss: {gen_loss_components['rec_loss']:.4f}, "
+                f"commit_loss: {gen_loss_components['commit_loss']:.4f}, "
+                f"smooth_loss: {gen_loss_components['smooth_loss']:.4f}, "
+                f"gen_loss: {gen_loss_components['gen_loss']:.4f}, "
+                f"diversity_loss: {gen_loss_components['diversity_loss']:.4f}, "
+                f"total_loss: {total_lossg:.4f}"
+                        )                
+                writer.add_scalar('generator_loss/rec_loss', gen_loss_components['rec_loss'], step)
+                writer.add_scalar('generator_loss/commit_loss', gen_loss_components['commit_loss'], step)
+                writer.add_scalar('generator_loss/smooth_loss', gen_loss_components['smooth_loss'], step)
+                writer.add_scalar('generator_loss/gen_loss', gen_loss_components['gen_loss'], step)
+                writer.add_scalar('generator_loss/diversity_loss', gen_loss_components['diversity_loss'], step)
+                writer.add_scalar('generator_loss/total_loss_gen', total_lossg, step)
             
             
-            disc_loss_components = loss_module.step_disc(output)
-            total_lossd = disc_loss_components['total_loss']
-            if step % config['logging']['step'] == 0:  
-                logging.info(f"DISC-LOSS---step/total: {step}/{num_steps} real_loss: {disc_loss_components['loss_real']}, fake_loss: {disc_loss_components['loss_fake']}, gp_loss: {disc_loss_components['grad_pen']}, total_loss: {disc_loss_components['total_loss']}")
+            
+            # ===== Discriminator Forward Pass =====
+            # Get text batch (every 2 steps) with auto-reset
+            if step % config['train']['discriminator_freq'] == 0:
                 
-                writer.add_scalar('Discriminator_loss/discriminator_total_loss', total_lossd, step)
-                writer.add_scalar('Discriminator_loss/discriminator_real_loss', disc_loss_components['loss_real'], step)
-                writer.add_scalar('Discriminator_loss/discriminator_fake_loss', disc_loss_components['loss_fake'], step)
-                writer.add_scalar('Discriminator_loss/discriminator_gp_loss', disc_loss_components['grad_pen'], step)
+                z_q_disc = z_q_disc.clone().detach()
+                disc_fake = models['discriminator'](z_q_disc, non_repeated_mask.unsqueeze(-1).clone().detach())
+                output['disc_fake'] = disc_fake
+                output["disc_fake_x"] = z_q_disc
+            
+                try:
+                    text, tmask = next(text_iter)
+                except StopIteration:
+                    text_iter = iter(text_loader)
+                    text, tmask = next(text_iter)
+                text = text.to(device)
+                tmask = tmask.to(device)
+                
+                text_emb = models['codebook'](text)
+                disc_real = models['discriminator'](text_emb, tmask.unsqueeze(-1))
+                output['disc_real'] = disc_real
+                output['disc_real_x'] = text_emb
+
+
         
+                # Discriminator training
+                loss_module.gan_loss.discriminator = models['discriminator']
+                
+                
+                disc_loss_components = loss_module.step_disc(output)
+                total_lossd = disc_loss_components['total_loss']
+                if step % config['logging']['step'] == 0:  
+                    logging.info(
+                    f"DISC-LOSS---step/total: {step}/{num_steps} "
+                    f"real_loss: {disc_loss_components['loss_real']:.4f}, "
+                    f"fake_loss: {disc_loss_components['loss_fake']:.4f}, "
+                    f"gp_loss: {disc_loss_components['grad_pen']:.4f}, "
+                    f"total_loss: {disc_loss_components['total_loss']:.4f}"
+                    )                    
+                    writer.add_scalar('Discriminator_loss/discriminator_total_loss', total_lossd, step)
+                    writer.add_scalar('Discriminator_loss/discriminator_real_loss', disc_loss_components['loss_real'], step)
+                    writer.add_scalar('Discriminator_loss/discriminator_fake_loss', disc_loss_components['loss_fake'], step)
+                    writer.add_scalar('Discriminator_loss/discriminator_gp_loss', disc_loss_components['grad_pen'], step)
+            
             
         # Backpropagation   
         if step % config['train']['discriminator_freq'] == 0:
             total_lossg += total_lossd
         total_lossg /= config['train']['gradient_accumulation_steps']
-        total_lossg.backward()
+        scaler.scale(total_lossg).backward() if config['train']['mixed_precision'] else total_lossg.backward()
         
 
         if step % config['train']['gradient_accumulation_steps'] == 0:
             # Gradient clipping
+            if config['train']['mixed_precision']:
+                for optimizer in optimizers.values():
+                    scaler.unscale_(optimizer)
+                
             max_grad_norm = config['train']['grad_clip']
             if step >= freeze_steps:
                 torch.nn.utils.clip_grad_norm_(models['encoder'].parameters(), max_grad_norm)
@@ -465,15 +471,27 @@ def train(models: Dict, optimizers: Dict, schedulers:Dict, speech_loader: DataLo
                     torch.nn.utils.clip_grad_norm_(models['codebook'].parameters(), max_grad_norm)
 
             # Optimizer step
-            if step >= freeze_steps:
-                optimizers['enc'].step()
-            optimizers['down'].step()
-            optimizers['dec'].step()
-            if step % config['train']['discriminator_freq'] == 0:
-                optimizers['disc'].step()
+            if config['train']['mixed_precision']:
                 if step >= freeze_steps:
-                    optimizers['codebook'].step()
-            
+                    scaler.step(optimizers['enc'])
+                scaler.step(optimizers['down'])
+                scaler.step(optimizers['dec'])
+                if step % config['train']['discriminator_freq'] == 0:
+                    scaler.step(optimizers['disc'])
+                    if step >= freeze_steps:
+                        scaler.step(optimizers['codebook'])
+                        
+                scaler.update()
+            else:
+                if step >= freeze_steps:
+                    optimizers['enc'].step()
+                optimizers['down'].step()
+                optimizers['dec'].step()
+                if step % config['train']['discriminator_freq'] == 0:
+                    optimizers['disc'].step()
+                    if step >= freeze_steps:
+                        optimizers['codebook'].step()
+                
             # scheduler step
             for scheduler in schedulers.values():
                 scheduler.step()    
